0bd423631fbb81bb55b199b2f6f8abb36fb16225
prlg <rlgranlund@rldimensions.com>
2022-01-05 14:13:08 -0600

    Working Commit

--- Puzzlr/Views/ContentView.swift
+++ Puzzlr/Views/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 
 struct ContentView: View {
+    @State private var translator: Translator = Translator()
     @State private var foundCode: String = ""
     @State private var rewardResponseIsVisible: Bool = false
     
@@ -23,16 +24,38 @@ struct ContentView: View {
                     .padding(.horizontal)
                     .frame(width: 100.0)
                     .multilineTextAlignment(.center)
+                    .disabled(rewardResponseIsVisible)
+                    .onSubmit {
+                        self.rewardResponseIsVisible = true
+                    }
                 Button(action: {
                     self.rewardResponseIsVisible = true
                 }) {
                     Text("Translate")
                 }
+                .disabled(rewardResponseIsVisible)
             }
             if rewardResponseIsVisible {
-                Text("Placeholder Reward Text")
-                    .padding(.top, 50.0)
-                    .foregroundColor(.purple)
+                let result = translator.translateCode(input: foundCode.uppercased())
+                if result.0 {
+                    Text(result.1)
+                        .padding(.top, 50.0)
+                        .foregroundColor(.purple)
+                }
+                else {
+                    Text(result.1)
+                        .padding(.top, 50.0)
+                        .foregroundColor(.red)
+                }
+                Button(action: {
+                    rewardResponseIsVisible.toggle()
+                    foundCode = ""
+                }) {
+                    Text("Try Again")
+                }
+                .padding(.top)
+                .disabled(!rewardResponseIsVisible)
+
             }
         }
     }
@@ -43,4 +66,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
             .preferredColorScheme(.dark)
     }
+    
+    
 }
